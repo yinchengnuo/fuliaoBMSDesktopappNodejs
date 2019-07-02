062b3c08849a7ad375ecd90d54f88acaa169fd45
yinchengnuo <yinnuo96@163.com>
2019-07-03 07:23:48 +0800

    init

---
+++ index.js
@@ -0,0 +1,128 @@
+const { app, BrowserWindow, Menu } = require('electron');
+app.commandLine.appendSwitch('ignore-connections-limit', 'pili-live-rtmp.hzwangjiao.com');
+
+// 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
+// 垃圾回收的时候，window对象将会自动的关闭
+let win
+
+const template = [
+  // { role: 'appMenu' }
+  ...(process.platform === 'darwin' ? [{
+    label: app.getName(),
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: '第一项',
+    submenu: [
+      { role: 'close' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: '第二项',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' }
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: '第三项',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forcereload' },
+      { role: 'toggledevtools' },
+      { type: 'separator' },
+      { role: 'resetzoom' },
+      { role: 'zoomin' },
+      { role: 'zoomout' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: '第四项',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' }
+    ]
+  },
+  {
+    role: '第五项',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { require('electron').shell.openExternalSync('https://electronjs.org') }
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
+function createWindow () {
+  // 创建浏览器窗口。
+  win = new BrowserWindow({
+    width: 1024,
+    height: 888,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // 加载index.html文件
+  win.loadFile('index.html')
+
+  // 打开开发者工具
+  win.webContents.openDevTools()
+
+  // 当 window 被关闭，这个事件会被触发。
+  win.on('closed', () => {
+    // 取消引用 window 对象，如果你的应用支持多窗口的话，
+    // 通常会把多个 window 对象存放在一个数组里面，
+    // 与此同时，你应该删除相应的元素。
+    win = null
+  })
+}
+
+// Electron 会在初始化后并准备
+// 创建浏览器窗口时，调用这个函数。
+// 部分 API 在 ready 事件触发后才能使用。
+app.on('ready', createWindow)
+
+// 当全部窗口关闭时退出。
+app.on('window-all-closed', () => {
+  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+  // 否则绝大部分应用及其菜单栏会保持激活。
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+  // 通常在应用程序中重新创建一个窗口。
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// 在这个文件中，你可以续写应用剩下主进程代码。
+// 也可以拆分成几个文件，然后用 require 导入。
